8a4e12ec980b705af8e1ce32b30b143805196c41
Lord <141478972+lordarera@users.noreply.github.com>
2024-03-22 00:39:48 +0700

    run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-21cdcecc-2e27-438d-8c23-605beebc241e/)
 
 
 echo "$curl_output" >> Update.tsx
